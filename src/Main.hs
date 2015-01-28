6dd38c7e715d96c08f24622a57fd44cf9e088f53
Justin (:flaviusb) Marsh <justin.marsh@flaviusb.net>
2015-01-28 23:35:49 +1300

    Tidying.

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ExtendedDefaultRules #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE DeriveDataTypeable #-}
 {-# OPTIONS_GHC -fno-warn-type-defaults #-}
 import Prelude hiding (FilePath)
@@ -8,12 +9,12 @@ import System.Console.CmdArgs
 import Data.Text as T
 default (T.Text)
 
-data Inputs = Inputs {
-    files :: [Text]
+data TestcaseFiles = TestcaseFiles {
+    testcase_files :: [Text]
   } deriving (Data,Typeable,Show,Eq)
 
-inputs = Inputs {
-    files = def &= args &= typ "FILES"
+input_command = TestcaseFiles {
+    testcase_files = def &= args &= typ "FILES"
   } &=
     verbosity &=
     help "Generates .t files from .t.in files" &=
@@ -22,5 +23,6 @@ inputs = Inputs {
             ,"To populate a t directory from the template files stored inside type:","  gen-testcases t/*.t.in"]
 
 main = shelly $ verbosely $ do
-  files <- liftIO $ cmdArgs inputs
+  input_struct <- liftIO $ cmdArgs input_command
+  let files = testcase_files input_struct
   echo $ pack $ show files
