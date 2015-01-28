518a0c798202e56f11c1844038ee2268c8565395
Justin (:flaviusb) Marsh <justin.marsh@flaviusb.net>
2015-01-29 01:05:06 +1300

    Build up and run the runnable lines.

--- src/Main.hs
+++ src/Main.hs
@@ -6,6 +6,7 @@
 import Prelude hiding (FilePath)
 import Shelly
 import System.Console.CmdArgs
+import Text.Shakespeare.Text (lt)
 import Data.Text as T
 default (T.Text)
 
@@ -36,8 +37,18 @@ filesplitter filename = do
   let test_input_file = TestInputFile { file_name = filename, file_couplets = split }
   return test_input_file
 
-main = shelly $ verbosely $ do
+coupletrunner couplet = do
+  result <- cmd $ fromText $ snd couplet
+  return (fst couplet, result)
+
+runfile file = do
+  new_couplets <- mapM coupletrunner $ file_couplets file
+  return TestInputFile { file_name = file_name file, file_couplets = new_couplets }
+
+main = shelly $ verbosely $ escaping False $ do
   input_struct <- liftIO $ cmdArgs input_command
   let files = testcase_files input_struct
   files_text <- mapM filesplitter files
+  files_out_data <- mapM runfile files_text
   echo $ pack $ show files_text
+  echo $ pack $ show files_out_data
