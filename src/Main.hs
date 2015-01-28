71e62a5ce596ce401e7e1d7122946c00577f00c5
Justin (:flaviusb) Marsh <justin.marsh@flaviusb.net>
2015-01-29 03:11:20 +1300

    Add templates, outfile. Some cleanup.

--- src/Main.hs
+++ src/Main.hs
@@ -1,12 +1,15 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ExtendedDefaultRules #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE DeriveDataTypeable #-}
 {-# OPTIONS_GHC -fno-warn-type-defaults #-}
 import Prelude hiding (FilePath)
 import Shelly
 import System.Console.CmdArgs
-import Text.Shakespeare.Text (lt)
+import System.FilePath (dropExtension)
+import Text.Shakespeare.Text (st)
+import Data.Monoid (mconcat)
 import Data.Text as T
 default (T.Text)
 
@@ -28,6 +31,39 @@ data TestInputFile = TestInputFile {
     file_couplets :: [(Text, Text)]
   } deriving (Data, Typeable, Show, Eq)
 
+data TestOutputFile = TestOutputFile {
+    file_name_out :: Text,
+    file_couplets_out :: [(Text, Text, Text)]
+  } deriving (Data, Typeable, Show, Eq)
+
+data OutFile = OutFile {
+    file_write_name :: Text,
+    file_contents :: Text
+  } deriving (Data, Typeable, Show, Eq)
+
+make_test :: (Text, Text, Text) -> Text
+make_test (name, command, expected_value) = [st|
+
+name "#{name}"
+first=`#{command}`
+first_expect='#{expected_value}'
+expect_eq "$first" "$first_expect"
+
+|]
+
+put_through_templates :: TestOutputFile -> OutFile
+put_through_templates file =
+  let rest = mconcat $ Prelude.map make_test $ file_couplets_out file
+      top = [st|#!/bin/bash
+
+. $(dirname $0)/test.sh
+
+plan #{show $ Prelude.length $ file_couplets_out file}
+
+|] in
+  OutFile {file_write_name = file_name_out file, file_contents = mconcat [top, rest]}
+  
+
 gettwo (a:b:lines) = (a, b):(gettwo lines)
 gettwo _           = []
 
@@ -39,16 +75,18 @@ filesplitter filename = do
 
 coupletrunner couplet = do
   result <- cmd $ fromText $ snd couplet
-  return (fst couplet, result)
+  return (fst couplet, snd couplet, result)
 
 runfile file = do
   new_couplets <- mapM coupletrunner $ file_couplets file
-  return TestInputFile { file_name = file_name file, file_couplets = new_couplets }
+  return TestOutputFile { file_name_out = pack $ dropExtension $ unpack $ file_name file, file_couplets_out = new_couplets }
 
 main = shelly $ verbosely $ escaping False $ do
   input_struct <- liftIO $ cmdArgs input_command
-  let files = testcase_files input_struct
+  let files = Prelude.filter (\file -> hasExt "in" $ fromText file) $ testcase_files input_struct
   files_text <- mapM filesplitter files
   files_out_data <- mapM runfile files_text
+  let each_file_out = Prelude.map put_through_templates files_out_data
   echo $ pack $ show files_text
   echo $ pack $ show files_out_data
+  echo $ pack $ show each_file_out
