523af23e21d56a7926c6fd3cf988438a6139c4d1
Justin (:flaviusb) Marsh <justin.marsh@flaviusb.net>
2015-01-29 00:24:34 +1300

    File couplets.

--- src/Main.hs
+++ src/Main.hs
@@ -11,7 +11,7 @@ default (T.Text)
 
 data TestcaseFiles = TestcaseFiles {
     testcase_files :: [Text]
-  } deriving (Data,Typeable,Show,Eq)
+  } deriving (Data, Typeable, Show, Eq)
 
 input_command = TestcaseFiles {
     testcase_files = def &= args &= typ "FILES"
@@ -22,10 +22,19 @@ input_command = TestcaseFiles {
     details ["gen-testcases generates .t files in the format I use from names and commands in .t.in files",""
             ,"To populate a t directory from the template files stored inside type:","  gen-testcases t/*.t.in"]
 
+data TestInputFile = TestInputFile {
+    file_name :: Text,
+    file_couplets :: [(Text, Text)]
+  } deriving (Data, Typeable, Show, Eq)
+
+gettwo (a:b:lines) = (a, b):(gettwo lines)
+gettwo _           = []
+
 filesplitter filename = do
   contents <- readfile $ fromText filename 
-  let split = T.lines contents
-  return split
+  let split = gettwo $ T.lines contents
+  let test_input_file = TestInputFile { file_name = filename, file_couplets = split }
+  return test_input_file
 
 main = shelly $ verbosely $ do
   input_struct <- liftIO $ cmdArgs input_command
